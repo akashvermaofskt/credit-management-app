f6754f5f52e9d5a7c9bbecdfd13e74df62b27b78
Akash Verma <akashvermaofsjs@gmail.com>
2019-06-09 21:50:33 +0530

    Updates

---
+++ precache-manifest.a9006b8bca4a9045c02703392da5ff57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70bb0bb15592881784a2744e3428137",
+    "url": "/credit-management-app/index.html"
+  },
+  {
+    "revision": "ddbdeef248c743940753",
+    "url": "/credit-management-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c82a2a8bdd198b7c6b63",
+    "url": "/credit-management-app/static/css/main.89f9edf2.chunk.css"
+  },
+  {
+    "revision": "ddbdeef248c743940753",
+    "url": "/credit-management-app/static/js/2.78747603.chunk.js"
+  },
+  {
+    "revision": "c82a2a8bdd198b7c6b63",
+    "url": "/credit-management-app/static/js/main.caf8ab6c.chunk.js"
+  },
+  {
+    "revision": "8d62839f336d0d75f787",
+    "url": "/credit-management-app/static/js/runtime~main.c0907523.js"
+  },
+  {
+    "revision": "ff61f89b74f982e1b4b49375223675e0",
+    "url": "/credit-management-app/static/media/logo.ff61f89b.png"
+  }
+]);
