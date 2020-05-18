64649d83ca495a2651920b29a2654b8812208d39
Akash Verma <akashvermaofsjs@gmail.com>
2020-05-18 16:55:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ let User = require('./user.model');
 
 app.use(cors());
 app.use(bodyParser.json());
-const connectionString = "mongodb+srv://akashvermaofskt:akv%401998@mycluster-xx5kl.mongodb.net/users"
+const connectionString = "mongodb+srv://akashvermaofskt:<password>@mycluster-xx5kl.mongodb.net/users"
 
 mongoose.connect(connectionString,{ useNewUrlParser: true });
 
